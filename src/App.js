e04b7c670dd2ce9b9f5490f96ebbe950c92043cc
carneiroDotDev <luiz@carneiro.dev>
2021-11-13 23:31:31 +0100

    Test lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import react from 'react'
 
 function App() {
+  console.log('')
   return (
     <div className="App">
       <header className="App-header">
